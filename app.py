5fab2a83561e0a624fa7a3e562c5f1aaa75bcc79
kumudugamlath <kumudugamlath2004@gmail.com>
2024-04-20 15:52:34 +0530

    added many things

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-#import libraries
+# import libraries
 import sqlite3
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 
-#configure app
+#c onfigure app
 app = Flask("__name__")
+app.run(debug=True)
 
-#configure session
+# configure session
 app.config["SESSION_PERMANANT"] = True
 app.config["SESSION_TYPE"] = "signed_cookies"
 Session(app)
@@ -16,7 +17,15 @@ db = sqlite3.connect("details.db")
 cursor = db.cursor()
 
 # index 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("layout.html")
+    if request.method == "POST":
+
+        code = request.form.get("code")
+        if not code:
+            return redirect("/")
+        
+        return redirect("/")
+    else:
+        return render_template("index.html")
     

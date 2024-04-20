edeebf0605c770f76103d5b12b0a9d4f8542921d
kumudugamlath <kumudugamlath2004@gmail.com>
2024-04-20 22:02:14 +0530

    added c

--- app.py
+++ app.py
@@ -1,27 +1,25 @@
 # import libraries
-import sqlite3
+import os
 import subprocess
-from flask import Flask, flash, redirect, render_template, request, session
-from flask_session import Session
+from flask import Flask, redirect, render_template, request
 
-#c onfigure app
+# configure app
 app = Flask("__name__")
 
-# configure session
-app.config["SESSION_PERMANANT"] = True
-app.config["SESSION_TYPE"] = "signed_cookies"
-Session(app)
-
-# configure db
-db = sqlite3.connect("details.db")
-cursor = db.cursor()
-
-# index 
-@app.route("/", methods=["GET", "POST"])
+# index
+@app.route("/")
 def index():
+    return render_template("index.html")
+
+# python
+@app.route("/python", methods=["GET", "POST"])
+def python():
     if request.method == "POST":
 
         code = request.form.get("code")
+
+        if code == "":
+            return render_template("python.html")
 
         try:
             process = subprocess.Popen(['python3', '-c', code], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
@@ -33,11 +31,52 @@ def index():
             else:
                 output = stderr
             
-            return render_template("index.html", output = output)
+            return render_template("python.html", output = output)
         
         except Exception as e:
-            return render_template("index.html", error = str(e))
+            return render_template("python.html", output = str(e))
         
     else:
-        return render_template("index.html")
+        return render_template("python.html")
     
+
+@app.route("/c", methods=["GET", "POST"])
+def c():
+    if request.method == "POST":
+
+        code = request.form.get("code")
+
+        if code == "":
+            return render_template("c.html")
+        
+        try:
+
+            with open("temp/c_temp.c", "w") as file:
+                file.write(code)
+            
+            gcc = subprocess.run(['gcc', 'temp/c_temp.c', '-o', 'temp/c_temp'], capture_output=True)
+
+            if gcc.returncode == 0:
+                process = subprocess.Popen(['./temp/c_temp'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                stdout, stderr = process.communicate()
+
+                if process.returncode == 0:
+                    output = stdout.decode()
+
+                else:
+                    output = stderr
+
+            else:
+                output = gcc.stderr.decode()
+
+
+            return render_template("c.html", output = output)
+        
+        except Exception as e:
+            return render_template("c.html", output = str(e))       
+        
+    else:
+        return render_template("c.html")
+    
+
+        

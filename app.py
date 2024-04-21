7a4b4f483e169eea4748a7d2f9a966b25d1d04c8
kumudugamlath <kumudugamlath2004@gmail.com>
2024-04-21 10:13:32 +0530

    trying to add SQL

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 # import libraries
 import os
+import sqlite3
 import subprocess
 from flask import Flask, redirect, render_template, request
 
 # configure app
 app = Flask("__name__")
+
+# configure db
+database =  sqlite3.connect("temp/sql.db")
+conn = database.cursor()
 
 # index
 @app.route("/")
@@ -39,7 +44,7 @@ def python():
     else:
         return render_template("python.html")
     
-
+# C
 @app.route("/c", methods=["GET", "POST"])
 def c():
     if request.method == "POST":
@@ -69,7 +74,6 @@ def c():
             else:
                 output = gcc.stderr.decode()
 
-
             return render_template("c.html", output = output)
         
         except Exception as e:
@@ -78,5 +82,26 @@ def c():
     else:
         return render_template("c.html")
     
+# SQL
+@app.route("/sql", methods=["GET", "POST"])
+def sql():
+    if request.method == "POST":
 
+        code = request.form.get("code")
+
+        if code == "":
+            return render_template("sql.html")
         
+        try:
+            
+            return render_template("sql.html", output = conn.fetchall())
+        
+        except Exception as e:
+            return render_template("sql.html", output = str(e))
+        
+    else:
+        return render_template("sql.html")
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
